956554df0bf166a0bb4ddcce00e39bfa690b536b
diaaelsherif <diaa.elsherif@msn.com>
2024-04-06 16:56:54 +0200

    designing the trips page

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,20 +1,14 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
+import { Trips } from "./components/Trip/Trips"
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+	{
+		index: true,
+		element: <Trips />
+	},
+	{
+		path: '/trips',
+		element: <Trips />
+	}
 ];
 
 export default AppRoutes;
---
+++ ClientApp/src/components/Trip/Trips.js
@@ -0,0 +1,64 @@
+﻿import React, { Component } from 'react';
+
+export class Trips extends Component
+{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            trips: [],
+            loading: false
+        }
+    }
+
+    renderAllTripsTable(trips) {
+        return (
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Date started</th>
+                        <th>Date completed</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>a</td>
+                        <td>a</td>
+                        <td>a</td>
+                        <td>a</td>
+                        <td> - </td>
+                    </tr>
+                    <tr>
+                        <td>b</td>
+                        <td>b</td>
+                        <td>b</td>
+                        <td>b</td>
+                        <td> - </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+
+        let content = this.state.loading ? (
+            <p>
+                <em>Loading...</em>
+            </p>
+        ) : (
+            this.renderAllTripsTable(this.state.trips)
+        )
+
+        return (
+            <div>
+                <h1>All trips</h1>
+                <p>Here you cann see all trips</p>
+                {content}
+            </div>
+        );
+    }
+}
